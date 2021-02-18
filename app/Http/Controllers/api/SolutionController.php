3206369a33d4af8575fbd0fc496c4b8ea3d7c9db
prakashgujarati <prakash@vsc.edu.in>
2021-02-18 18:29:27 +0530

    update in solution

--- app/Http/Controllers/api/SolutionController.php
+++ app/Http/Controllers/api/SolutionController.php
@@ -14,7 +14,65 @@ use App\Models\semester;
 
 class SolutionController extends Controller
 {
+	public function solutionList(Request $request){
 
+    	$rules = array(
+            'unit_id' => 'required'
+        );
+        $messages = array(
+        	'unit_id.required' => 'Please enter unit id.'
+        );
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            $msg = $validator->messages();
+            return ['status' => "false",'msg' => $msg];
+        }
+
+        $chkunit = unit::where(['id' => $request->unit_id,'status' => 'Active'])->first();
+
+        if(empty($chkunit)){
+        	return response()->json([
+    			"code" => 400,
+			  	"message" => "Standard not found.",
+			  	"data" => [],
+	        ]);
+        }
+        else{
+
+        	$getdata = unit::where(['unit_id' => $request->unit_id,'status' => 'Active'])->get();
+        	
+	    	if(count($getdata) > 0){
+	    		$data=[];$getdata=[];
+				$title="";
+
+	    		$getdata = solution::where(['unit_id' => $getdata->id,'status' => 'Active'])->get();
+	    			$solutiondata=[];
+	    			foreach ($getdata as $value1) {
+	    				$image = env('APP_URL')."/upload/solution/".$value1->image;
+						$title = $value1->label;
+	    				$solutiondata[] = ['id' => $value1->id,'question' => $value1->question,'answer' => $value1->answer,'marks' => $value1->marks,'image' => $image,'label' => $value1->label];
+	    			}
+
+	    			$data[] = ['id' => $value->id,'unit_title' =>$title,'solution' => $solutiondata];
+	    		
+	    		return response()->json([
+	    			"code" => 200,
+				  	"message" => "success",
+				  	"data" => $data,
+		        ]);
+	    	}
+	    	else{
+	    		return response()->json([
+	    			"code" => 400,
+				  	"message" => "Solution details not found.",
+	 			  	"data" => [],
+		        ]);
+	    	}		
+        }
+    }
+	/*
 	public function solutionList(Request $request){
 
     	$rules = array(
@@ -91,5 +149,5 @@ class SolutionController extends Controller
 		        ]);
 	    	}		
         }
-    }
+    }*/
 }
