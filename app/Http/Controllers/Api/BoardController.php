23330416b5659ddef09a478dabc9f826558a9966
prakashgujarati <prakash@vsc.edu.in>
2021-03-18 21:25:57 +0530

    api update

--- app/Http/Controllers/Api/BoardController.php
+++ app/Http/Controllers/Api/BoardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Board;
+use App\Models\Medium;
 use DB;
 
 class BoardController extends Controller
