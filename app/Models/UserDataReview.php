22c901176908cf00424efd911e1921ff0be27c8d
vagadiyashyam <shyamvagadiya@gmail.com>
2021-03-24 21:59:36 +0530

    changes till now

---
+++ app/Models/UserDataReview.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserDataReview extends Model
+{
+    use HasFactory;
+}
