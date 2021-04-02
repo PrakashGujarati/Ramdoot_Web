948a2c5c9069b7a8f5b6c9ab22aa875e174ce295
keyurschauhan <65502373+keyurschauhan@users.noreply.github.com>
2021-04-02 07:49:18 +0530

    unit counter api

--- app/Http/Controllers/Api/UnitController.php
+++ app/Http/Controllers/Api/UnitController.php
@@ -11,7 +11,16 @@ use App\Models\Standard;
 use App\Models\Subject;
 use DB;
 use Validator;
+use App\Models\Book;
+use App\Models\Note;
+use App\Models\Paper;
+use App\Models\Worksheet;
 use App\Models\Question;
+use App\Models\Feature;
+use App\Models\Videos;
+use App\Models\Material;
+use App\Models\Solution;
+use App\Models\pdf_view;
 
 class UnitController extends Controller
 {
@@ -20,11 +29,13 @@ class UnitController extends Controller
             'standard_id' => 'required',
             'semester_id' => 'required',
             'subject_id' => 'required',
+            'feature_id' => 'required'
         );
         $messages = array(
             'standard_id.required' => 'Please enter standard id.',
             'semester_id.required' => 'Please enter semester id.',
-            'subject_id.required' => 'Please enter subject id.'
+            'subject_id.required' => 'Please enter subject id.',
+            'feature_id.required' => 'Please enter feature id.'
         );
 
         $validator = Validator::make($request->all(), $rules, $messages);
@@ -63,12 +74,115 @@ class UnitController extends Controller
                 $getdata = Unit::where(['subject_id' => $request->subject_id,'standard_id' => $request->standard_id,'semester_id' => $request->semester_id,'status' => 'Active'])->get();
                 
                 if(count($getdata) > 0){
-                    $data=[];
+                    $data=[];$totalcount=0;
                     foreach ($getdata as $value) {
+                        $readcount=0;
                         $url = env('APP_URL')."/upload/subject/url/".$value->url;
                         $thumbnail = env('APP_URL')."/upload/subject/thumbnail/".$value->thumbnail;
+
+                        if($request->feature_id != 0){
+
+                            if($request->feature_id == 3){
+                                $totalcount = Book::where('unit_id',$value->id)->count();
+                                $books = Book::where('unit_id',$value->id)->get();
+                                foreach($books as $book)
+                                {
+                                    $c = pdf_view::where(['type' => 'Textbook','type_id' => $book->id])->count();
+                                    if($c)
+                                    {
+                                        $readcount++;
+                                    }
+                                }
+                            }
+                            elseif ($request->feature_id == 9) {
+                                $totalcount = Note::where('unit_id',$value->id)->count();
+                                $notes = Note::where('unit_id',$value->id)->get();
+                                foreach($notes as $note)
+                                {
+                                    $c = pdf_view::where(['type' => 'Note','type_id' => $note->id])->count();
+                                    if($c)
+                                    {
+                                        $readcount++;
+                                    }
+                                }
+                            }
+                            elseif ($request->feature_id == 7) {
+                                $totalcount = Worksheet::where('unit_id',$value->id)->count();
+                                $worksheets = Worksheet::where('unit_id',$value->id)->get();
+                                foreach($worksheets as $worksheet)
+                                {
+                                    $c = pdf_view::where(['type' => 'Worksheet','type_id' => $worksheet->id])->count();
+                                    if($c)
+                                    {
+                                        $readcount++;
+                                    }
+                                }
+                            }
+                            elseif ($request->feature_id == 6) {
+                                $totalcount = Paper::where('unit_id',$value->id)->count();
+                                $papers = Paper::where('unit_id',$value->id)->get();
+                                foreach($papers as $paper)
+                                {
+                                    $c = pdf_view::where(['type' => 'Paper','type_id' => $paper->id])->count();
+                                    if($c)
+                                    {
+                                        $readcount++;
+                                    }
+                                }
+                            }
+                            elseif ($request->feature_id == 2){
+                                $totalcount = Videos::where('unit_id',$value->id)->count();
+                                $videos = Videos::where('unit_id',$value->id)->get();
+                                foreach($videos as $video)
+                                {
+                                    $c = pdf_view::where(['type' => 'Video','type_id' => $video->id])->count();
+                                    if($c)
+                                    {
+                                        $readcount++;
+                                    }
+                                }   
+                            }
+                            elseif ($request->feature_id == 5){
+                                
+                                $totalcount = Material::where('unit_id',$value->id)->count();
+                                $materials = Material::where('unit_id',$value->id)->get();
+                                foreach($materials as $material)
+                                {
+                                    $c = pdf_view::where(['type' => 'Material','type_id' => $material->id])->count();
+                                    if($c)
+                                    {
+                                        $readcount++;
+                                    }
+                                }   
+                            }
+                            elseif($request->feature_id == 8){
+                                $totalcount = Question::where('unit_id',$value->id)->count();
+                                $Questions = Question::where('unit_id',$value->id)->get();
+                                foreach($Questions as $Question)
+                                {
+                                    $c = pdf_view::where(['type' => 'MCQ','type_id' => $Question->id])->count();
+                                    if($c)
+                                    {
+                                        $readcount++;
+                                    }
+                                }
+                            }
+                            elseif($request->feature_id == 4){
+                                $totalcount = Solution::where('unit_id',$value->id)->count();
+                                $solutions = Solution::where('unit_id',$value->id)->get();
+                                foreach($solutions as $solution)
+                                {
+                                    $c = pdf_view::where(['type' => 'Exercise','type_id' => $solution->id])->count();
+                                    if($c)
+                                    {
+                                        $readcount++;
+                                    }
+                                }
+                            }
+                        }
+
                         $unitcount = Question::where('unit_id',$value->id)->count();
-                        $data[] = ['id' => $value->id,'title' => $value->title,'sub_title' => $value->description,'url' => $url,'thumbnail' => $thumbnail,"pages"=> $value->pages,"total_questions"=>$unitcount];
+                        $data[] = ['id' => $value->id,'title' => $value->title,'sub_title' => $value->description,'url' => $url,'thumbnail' => $thumbnail,"pages"=> $value->pages,"total_questions"=>$unitcount,"total_count"=>$totalcount,"readcount"=>$readcount];
                     }
 
                     return response()->json([
