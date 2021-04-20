2e865cc1f4e7551145978ace23ad8277f24bbd53
Prakash Gujarati <prakash@vsc.edu.in>
2021-04-20 23:34:58 +0530

    Update standard revert
    Update standard revert

--- app/Http/Controllers/Api/StandardController.php
+++ app/Http/Controllers/Api/StandardController.php
@@ -36,12 +36,12 @@ class StandardController extends Controller
 	    		$data=[];$getdata=[];
 	    		foreach ($getboard_details as $value) {                    
 	    			$getdata = Standard::select('id','standard','thumbnail')->where(['medium_id' => $request->medium_id,'section' => $value->section])->orderBy('order_no','asc')->get();
-	    			foreach ($getdata as $key => $std_value) 
+	    			/*foreach ($getdata as $key => $std_value) 
                     {
                        $thumbnail = env('APP_URL')."/upload/standard/thumbnail/".$std_value->thumbnail;
                        $stdArray[] = ['id' => $std_value->id,'standard' => $std_value->standard,'thumbnail' => $thumbnail];
-                    }
-                    $data[] = ['id' => $value->id,'section' => $value->section,'standard' => $stdArray];
+                    }*/
+                    $data[] = ['id' => $value->id,'section' => $value->section,'standard' => $getdata];
 	    		}
 
 	    		return response()->json([
