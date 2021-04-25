daedc2ec390255548085efddb61df0adbbdc39ae
DESKTOP-I1O9SR2\PrakashGujarati <prakash@vsc.edu.in>
2021-04-25 14:51:28 +0530

    update for deploy

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DeployController;
 use App\Http\Controllers\BoardController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\StandardController;
