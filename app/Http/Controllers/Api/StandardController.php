410af8cfd4b7773532664e9327be93bfcbea9ba2
DESKTOP-I1O9SR2\PrakashGujarati <prakash@vsc.edu.in>
2021-05-09 14:45:37 +0530

    standarlist bug

--- app/Http/Controllers/Api/StandardController.php
+++ app/Http/Controllers/Api/StandardController.php
@@ -10,7 +10,7 @@ use Validator;
 use App\Models\Medium;
 class StandardController extends Controller
 {
-	/*
+	
     public function standardList(Request $request){
 
     	$rules = array(
@@ -37,21 +37,7 @@ class StandardController extends Controller
 	    		foreach ($getstandard_details as $value) {                    
 	    			$getdata = Standard::select('id','standard','thumbnail','thumbnail_file_type')->where(['medium_id' => $request->medium_id,'section' => $value->section])->orderBy('order_no','asc')->get();
 
-	    			foreach ($getdata as $sub_key => $sub_value) 
-	                {
-	                	$thumbnail='';
-	                	if($sub_value->thumbnail){
-	                		if($sub_value->thumbnail_file_type == "Server"){
-		                		$thumbnail =  env('APP_URL')."/upload/standard/thumbnail/".$sub_value->thumbnail;		
-		                	}
-		                	else{
-		                		$thumbnail =  $sub_value->thumbnail;	
-		                	}	
-	                	}
-	                	
-	                	$standard_data[] = ['id' => $sub_value->id,'standard' => $sub_value->standard,'thumbnail' => $thumbnail];	
-		    		}
-
+	    			
 		    		$data[] = ['id' => $value->id,'section' => $value->section,'standard' => $standard_data,'sub_title' => $value->sub_title];
 
 		    		return response()->json([
@@ -60,61 +46,6 @@ class StandardController extends Controller
 					  	"data" => $data,
 			        ]);
 	    		}
-	    	}
-	    	else{
-	    		return response()->json([
-	    			"code" => 400,
-				  	"message" => "Standard not found.",
-				  	"data" => [],
-		        ]);
-	    	}	
-	    }
-        else{
-        	return response()->json([
-    			"code" => 400,
-			  	"message" => "Board details not found.",
-			  	"data" => [],
-	        ]);
-        }
-
-    	
-
-    }*/
-
-	public function standardList(Request $request){
-
-    	$rules = array(
-            'medium_id' => 'required'
-        );
-        $messages = array(
-            'medium_id.required' => 'Please Enter Medium Id.'
-        );
-
-        $validator = Validator::make($request->all(), $rules, $messages);
-
-        if ($validator->fails()) {
-            $msg = $validator->messages();
-            return ['status' => "false",'msg' => $msg];
-        }
-
-        $chkmedium = Medium::where(['id' => $request->medium_id,'status' => 'Active'])->first();
-        if($chkmedium){
-        	$getstandard_details = Standard::where(['medium_id' => $request->medium_id,'status' => 'Active'])->select('id','section')->groupBy('section')->get();
-
-	    	if(count($getstandard_details) > 0){
-
-	    		$data=[];$getdata=[];
-	    		foreach ($getstandard_details as $value) {                    
-	    			$getdata = Standard::select('id','standard','thumbnail')->where(['medium_id' => $request->medium_id,'section' => $value->section])->orderBy('order_no','asc')->get();
-	    			
-                    $data[] = ['id' => $value->id,'section' => $value->section,'standard' => $getdata,'sub_title' => $value->sub_title];
-	    		}
-
-	    		return response()->json([
-	    			"code" => 200,
-				  	"message" => "success",
-				  	"data" => $data,
-		        ]);
 	    	}
 	    	else{
 	    		return response()->json([
