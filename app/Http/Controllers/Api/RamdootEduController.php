db2cadc70f9e910913bf08cf9aafc0790082dfa8
Prakash Gujarati <prakash@vsc.edu.in>
2021-06-12 15:05:04 +0530

    Update classroom delete
    Update classroom delete

--- app/Http/Controllers/Api/RamdootEduController.php
+++ app/Http/Controllers/Api/RamdootEduController.php
@@ -268,9 +268,10 @@ class RamdootEduController extends Controller
             return ['status' => "false",'msg' => $msg];
         }
 
-        $check_class = Classroom::where(['id' => $request->class_id]);
+        $check_class = Classroom::where(['id' => $request->class_id])->first();
         $check_class->status = "Deleted";
         $check_class->save();
+        
         if($check_class){
             Classroom::where(['id' => $request->class_id])->delete();
             return response()->json([
