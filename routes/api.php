b4e25722e9ac7d0497324d2fed35476052c6beae
keyurschauhan <65502373+keyurschauhan@users.noreply.github.com>
2021-07-20 19:57:57 +0530

    changes fix

--- routes/api.php
+++ routes/api.php
@@ -135,7 +135,6 @@ Route::group(['middleware'=>'auth:api'], function()
     Route::post('attendance_list', [RamdootEduController::class, 'attendanceList']);
     //Route::post('get_lecture', [RamdootEduController::class, 'getLecture']);
     //Route::post('edit_attendance', [RamdootEduController::class, 'editAttendance']);
-
     Route::post('add_institute', [RamdootEduController::class, 'addInstitute']);
     Route::post('institute_list', [RamdootEduController::class, 'instituteList']);
     
